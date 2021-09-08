25b21557ba877312565d06275021f604f03d0169
Nahid <nahidswe@gmail.com>
2021-09-08 14:47:59 +0600

    First commit from master branch

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Start() variables
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+    public int cherries = 0;
+
+    //FSM
+    private enum State {idle, running, jumping, falling}
+    private State state = State.idle;
+    
+    //Inspector variables
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //  print("Inside Start Method.");
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement();
+
+        AnimationState();
+        anim.SetInteger("state", (int)state);
+
+    }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectable")
+        {
+            Destroy(collision.gameObject);
+            cherries += 1;
+        }
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        //moving right
+        if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+        //moving left
+        else if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+    private void AnimationState()
+    {
+        if(state == State.jumping)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+}
+
+
+
+
+
+
+/*
+ 
+if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(5f, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+           
+            rb.velocity = new Vector2(-5f, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
+            
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -10f);
+        }
+
+ */
